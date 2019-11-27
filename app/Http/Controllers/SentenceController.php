76c862faaa2b0e99d9fbb8a9bd7efb14a8889380
William <work@suppo.fr>
2019-11-27 17:46:29 +0100

    On change l'ordre d'affichage des sentences.

--- app/Http/Controllers/SentenceController.php
+++ app/Http/Controllers/SentenceController.php
@@ -15,7 +15,7 @@ class SentenceController extends Controller
      */
     public function index()
     {
-        $sentences = Sentence::orderBy('exposed_at', 'asc')->get();
+        $sentences = Sentence::orderBy('exposed_at', 'desc')->get();
 
         return view('sentences.index', compact('sentences'));
     }
