9bd44eadb66a94a331b4be79cd2c0c5791ab7e3e
William <work@suppo.fr>
2019-11-28 09:27:01 +0100

    Lancement du motd à 9h00 et 14h00.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('motd')->dailyAt('08:00');
+         $schedule->command('motd')->twiceDaily(9, 14);
     }
 
     /**
